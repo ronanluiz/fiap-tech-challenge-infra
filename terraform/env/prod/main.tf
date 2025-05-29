e396da5d2f355f10572fa50867373fcd32833262
Ronan Luiz Araújo <ronanluiz@gmail.com>
2025-05-29 07:12:04 -0300

    ci: ajusta configurações para produção

--- terraform/env/prod/main.tf
+++ terraform/env/prod/main.tf
@@ -1,7 +1,7 @@
 module "prod" {
   source       = "../../shared"
   region       = "us-east-1"
-  environment  = "dev"
+  environment  = "prod"
   eks_name     = "prod-eks-cluster"
   vpc_name     = "prod-vpc"
   project_name = "prod-tc-infra"
